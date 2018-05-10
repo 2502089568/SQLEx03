ada0ad177a113d14492d751ff25bb9395dc7b2b2
2502089568 <1508950473@qq.com>
2018-05-10 20:29:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zz/sqlex03/DBManager.java
@@ -0,0 +1,110 @@
+package com.example.zz.sqlex03;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.zz.sqlex03.dao.DaoMaster;
+import com.example.zz.sqlex03.dao.DaoSession;
+import com.example.zz.sqlex03.dao.UserDao;
+
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import java.util.List;
+
+public class DBManager {
+    private final static String dbName = "test_db";
+    private static DBManager mInstance;
+    private DaoMaster.DevOpenHelper openHelper;
+    private Context context;
+
+    public DBManager(Context context){
+        this.context=context;
+        openHelper = new DaoMaster.DevOpenHelper(context,dbName,null);//第三个参数为Cursor();
+    }
+
+    public  static DBManager getInstance(Context context){
+        if (mInstance == null) {//双重检查锁定
+            synchronized (DBManager.class) {
+                if (mInstance == null) {
+                    mInstance = new DBManager(context);
+                }
+            }
+        }
+        return mInstance;
+    }
+
+    /**
+     * 获取可读数据库
+     */
+    private SQLiteDatabase getReadableDatabase(){
+        if(openHelper ==null ){
+            openHelper = new DaoMaster.DevOpenHelper(context,dbName,null);
+        }
+        SQLiteDatabase db = openHelper.getReadableDatabase();//分读数据库和写数据库
+        return db;
+    }
+
+    /**
+     * 获取可写数据库
+     */
+    private SQLiteDatabase getWritableDatabase() {
+        if (openHelper == null) {
+            openHelper = new DaoMaster.DevOpenHelper(context, dbName, null);
+        }
+        SQLiteDatabase db = openHelper.getWritableDatabase();
+        return db;
+    }
+
+    //插入一条记录
+    public void inserUser(User user){
+        if(user == null){ return;}
+        DaoMaster daoMaster = new DaoMaster(getWritableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        UserDao userDao = daoSession.getUserDao();
+        userDao.insert(user);
+    }
+
+    //插入用户集合
+    public void insertUserList(List<User> users){
+        if(users == null||users.isEmpty()){ return;}
+        DaoMaster daoMaster = new DaoMaster(getWritableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        UserDao userDao = daoSession.getUserDao();
+        userDao.insertInTx(users);
+    }
+
+    //删除一条记录
+    public void deleterUser(User user){
+        DaoMaster daoMaster = new DaoMaster(getWritableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        UserDao userDao = daoSession.getUserDao();
+        userDao.delete(user);
+    }
+
+    //更新一条记录
+    public void updateUser(User user){
+        DaoMaster daoMaster = new DaoMaster(getWritableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        UserDao userDao = daoSession.getUserDao();
+        userDao.update(user);
+    }
+
+    public List<User> queryUserList(){
+        DaoMaster daoMaster = new DaoMaster(getReadableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        UserDao userDao = daoSession.getUserDao();
+        QueryBuilder<User> qb = userDao.queryBuilder();
+        List<User>list = qb.list();
+        return list;
+    }
+    public List<User> queryUserList(int age) {
+        DaoMaster daoMaster = new DaoMaster(getReadableDatabase());
+        DaoSession daoSession = daoMaster.newSession();
+        UserDao userDao = daoSession.getUserDao();
+        QueryBuilder<User> qb = userDao.queryBuilder();
+        qb.where(UserDao.Properties.Age.gt(age)).orderAsc(UserDao.Properties.Age);
+        List<User> list = qb.list();
+        return list;
+    }
+
+}
